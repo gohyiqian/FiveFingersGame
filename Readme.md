baa01903761588ae5a076ae04781144102272c34
gyq765 <kenji.gohyiqian@gmail.com>
2021-07-15 16:02:16 +0800

    Added Linear Gradient

--- Readme.md
+++ Readme.md
@@ -2,25 +2,46 @@
 Format: ![Alt Text](url)
 
 # My Mini Game
-
+## An Overview
 Hello.
 
 This is a simple adaptation of the popular *Guitar Hero* Game first released in 2005.
-Players match colored notes that scroll down on screen to score points.
+Players will time the descending target and receive points when they keypress or mouseclick 'A','S','D','F','G' at the correct timing.
 
 ![Guitar Image](/images/screenshot.jpg)
 Format: ![Alt Text](url)
 
-<canvas> is an HTML element that allows a user to create graphics using Javascript. It can be used to create animations, interactive graphics, and browser games.
+The game is created using custom functions of HTML canvas element.
+Canvas is an HTML element that allows a user to create graphics, on the fly, using Javascript. It can be used to create animations, interactive graphics, and browser games.
+
+Basic Animation Steps using canvas elements:
+1. Draw the shapes to be animated
+2. Define motion (translate/rotate/scale)
+3. Clear the canvas
+4. Update/Restore the canvas using either: setInterval, setTimeout or requestAnimationFrame functions
 
 ## Some To-Do List I have set for myself
 
+
+- [ ] Add Start Scene
+- [ ] Add How to Play Scene
+- [ ] Add Playing Scene
+- [ ] Add Game Over Scene
 - [x] Create a canvas and add 5 buttons
 - [x] Add mouse-click & keypress eventlisteners
 - [x] Add corresponding colors when button is activated
 - [x] Add some music effects when button is activated correctly vs wrongly
+- [ ] Add requestAnimationFrame function to loop animations
 - [ ] Define a threshold for 'correctness'
-- [ ] Add scoring based of timing of activating the buttons
+- [ ] Add scoring based of # of correct timing of activating the buttons
+- [ ] Add gradient trail for movement
+
+## Implementation
+
+## Future Improvements
+- [ ] Add different songs
+- [ ] Add combos or streaks
+- [ ] Add different levels
 
 
 ###### This is an <h6> tag
@@ -56,11 +77,6 @@ I think you should use an
         $.facebox({div:'#foo'})
       }
     }
-
-- [x] @mentions, #refs, [links](), **formatting**, and <del>tags</del> supported
-- [x] list syntax required (any unordered or ordered list supported)
-- [x] this is a complete item
-- [ ] this is an incomplete item
 
 First Header | Second Header
 ------------ | -------------
--- script.js
+++ script.js
@@ -1,87 +1,128 @@
-// 'use strict'
+'use strict'
 
-function startGame() {
-  redGamePiece = new component(50, 50, "red", 15, 10);
-  blueGamePiece = new component(50, 50, "blue", 90, 10);
-  greenGamePiece = new component(50, 50, "green", 175, 10);
-  yellowGamePiece = new component(50, 50, "yellow", 255, 10);
-  purpleGamePiece = new component(50, 50, "purple", 335, 10);
-  line1 = new trackLines(40, 0, 40, 500);
-  line2 = new trackLines(120, 0, 120, 500);
-  line3 = new trackLines(200, 0, 200, 500);
-  line4 = new trackLines(280, 0, 280, 500);
-  line5 = new trackLines(360, 0, 360, 500);
-  myGameArea.start();
+// define Canvas
+let canvas = document.createElement("canvas");
+let ctx = canvas.getContext("2d");
+const w = canvas.width = 400;
+const h = canvas.height = 500;
+
+
+// function startScene() {
+
+// }
+
+// function instructionScene() {
+
+// }
+
+// document.addEventListener("click", function () {
+//   ctx.rect(0, 0, w, h);
+//   ctx.fill();
+// })
+
+function myGameArea() {
+  document.body.insertBefore(canvas, document.body.childNodes[0]);
+  setInterval(animateGameArea, 1);
 }
 
-let myGameArea = {
-  canvas: document.createElement("canvas"),
-  start: function () {
-    this.canvas.width = 400;
-    this.canvas.height = 500;
-    this.context = this.canvas.getContext("2d"); //use canvas methods to draw in 2D
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 20);
-  },
-  clear: function () {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+// create a class blueprint for moving components
+class component {
+  constructor(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.x = x;
+    this.y = y;
   }
-}
-
-function component(width, height, color, x, y) {
-  this.width = width;
-  this.height = height;
-  this.x = x;
-  this.y = y;
-  this.update = function () {
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
+  update() {
+    let grd = ctx.createLinearGradient(0, 0, 0, 500);
+    grd.addColorStop(0, "white");
+    grd.addColorStop(1, this.color);
+    ctx.fillStyle = grd;
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
 
-function trackLines(x1, y1, x2, y2) {
-  this.x1 = x1;
-  this.y1 = y1;
-  this.x2 = x2;
-  this.y2 = y2;
-  this.strokeStyle = "black";
-  this.drawLine = function () {
+
+// creating track lines
+class trackLines {
+  constructor(x1, y1, x2, y2, strokeStyle) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.strokeStyle = "black";
+  }
+  drawLine() {
     ctx.beginPath();
-    // ctx.lineWidth = "2";
-    // ctx.strokeStyle = "black";
     ctx.moveTo(this.x1, this.y1);
     ctx.lineTo(this.x2, this.y2);
     ctx.stroke();
-    // ctx.closePath();
+    ctx.closePath();
   }
 }
 
-// function randomSpeed(){
-//   let power = Math.floor(Math.random()*10)
-//   let num = Math.floor(Math.random()*5+ power)
-//   console.log(num)
-// }
-// randomSpeed()
+// generating class instances
+const redGamePiece = new component(50, 50, "red", 15, 10);
+const blueGamePiece = new component(50, 50, "blue", 90, 10);
+const greenGamePiece = new component(50, 50, "green", 175, 10);
+const yellowGamePiece = new component(50, 50, "yellow", 255, 10);
+const purpleGamePiece = new component(50, 50, "purple", 335, 10);
 
-function updateGameArea() {
+const line1 = new trackLines(40, 0, 40, 500);
+const line2 = new trackLines(120, 0, 120, 500);
+const line3 = new trackLines(200, 0, 200, 500);
+const line4 = new trackLines(280, 0, 280, 500);
+const line5 = new trackLines(360, 0, 360, 500);
 
-  // can improve this random movement
+
+function randomSpeed() {
+  let power = Math.floor(Math.random() * 10)
+  let num = Math.floor(Math.random() * 5 + power)
+  console.log(power)
+  console.log(num)
+}
+randomSpeed()
+
+let power = '-' + Math.floor(Math.random() * 1000) + 'px'
+console.log(power)
+
+// document.getElementById("key").animate([
+//   // keyframes
+//   {
+//     transform: 'translateY(0px)'
+//   },
+//   {
+//     transform: 'translateY(-200px)'
+//   }
+// ], {
+//   // timing options
+//   duration: 500,
+//   iterations: Infinity
+// });
+
+
+function animateGameArea() {
+  // can improve this random speed
   let num = Math.floor((Math.random() * 5))
   let num2 = Math.floor((Math.random() * 6))
   let num3 = Math.floor((Math.random() * 3))
   let num4 = Math.floor((Math.random() * 4.5))
-  myGameArea.clear();
+  let num5 = Math.floor((Math.random() * 4))
+
   redGamePiece.y += num;
   blueGamePiece.y += num2;
   greenGamePiece.y += num3;
   yellowGamePiece.y += num4;
-  purpleGamePiece.y += num;
+  purpleGamePiece.y += num5;
+
+  ctx.clearRect(0, 0, w, h);
   redGamePiece.update();
   blueGamePiece.update();
   greenGamePiece.update();
   yellowGamePiece.update();
   purpleGamePiece.update();
+
   line1.drawLine();
   line2.drawLine();
   line3.drawLine();
@@ -89,7 +130,26 @@ function updateGameArea() {
   line5.drawLine();
 }
 
-startGame()
+myGameArea()
+// const element = document.getElementById('key');
+// let start;
+// function step(timestamp) {
+//   if (start === undefined)
+//     start = timestamp;
+//   const elapsed = timestamp - start;
+
+//   // `Math.min()` is used here to make sure that the element stops at exactly 400px.
+//   element.style.transform = 'translateY(' + Math.min(0.1 * elapsed, 400) + 'px)';
+
+//   if (elapsed < 4000) { // Stop the animation after 2 seconds
+//     window.requestAnimationFrame(step);
+//   }
+// }
+
+// window.requestAnimationFrame(step);
+
+let gameScore = 0;
+
 
 
 // EventListeners
@@ -111,33 +171,32 @@ document.addEventListener("keypress", function (event) {
   buttonAnimation(event.key);
 });
 
-
 function makeSound(key) {
 
   switch (key) {
     case "a":
-      let tom1 = new Audio("sounds/correctSound.mp3");
-      tom1.play();
+      let btnA = new Audio("sounds/correctSound.mp3");
+      btnA.play();
       break;
 
     case "s":
-      let tom2 = new Audio("sounds/correctSound.mp3");
-      tom2.play();
+      let btnB = new Audio("sounds/correctSound.mp3");
+      btnB.play();
       break;
 
     case "d":
-      let tom3 = new Audio('sounds/correctSound.mp3');
-      tom3.play();
+      let btnD = new Audio('sounds/correctSound.mp3');
+      btnD.play();
       break;
 
     case "f":
-      let tom4 = new Audio('sounds/correctSound.mp3');
-      tom4.play();
+      let btnF = new Audio('sounds/correctSound.mp3');
+      btnF.play();
       break;
 
     case "g":
-      let snare = new Audio('sounds/wrongSound.mp3');
-      snare.play();
+      let btnH = new Audio('sounds/wrongSound.mp3');
+      btnH.play();
       break;
 
     default:
@@ -146,7 +205,7 @@ function makeSound(key) {
   }
 }
 
-// some button effects
+// some button effects by changing css class
 function buttonAnimation(key) {
   let activeButton = document.querySelector("." + key);
   activeButton.classList.add("pressed_" + key);
@@ -154,5 +213,4 @@ function buttonAnimation(key) {
   setTimeout(function () {
     activeButton.classList.remove("pressed_" + key);
   }, 200);
-
 }
