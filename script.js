4c7f11e4b8637af1ba0931aa3e58bab773efdc8d
gyq765 <kenji.gohyiqian@gmail.com>
2021-07-23 23:40:55 +0800

    Working on Create Song Portion

--- script.js
+++ script.js
@@ -7,26 +7,26 @@ const cw = canvas.width = 380;
 const ch = canvas.height = 500;
 let gameBlocks = [];
 let lines = [];
-let content = document.getElementById('move');
+let startgame = document.getElementById('move');
 let gameStatus;
 
-content.addEventListener('click', function (e) {
-  if (content.innerHTML == "PRESS TO START" || content.innerHTML == "GAME OVER") {
-    gameStatus = window.setInterval(animateGame, 6);
+startgame.addEventListener('click', function (e) {
+  if (startgame.innerHTML == "PRESS TO PLAY DEFAULT SONG" || startgame.innerHTML == "GAME OVER") {
+    gameStatus = window.setInterval(animateGame, 8);
     //gameStatus = animateGame();
     //document.getElementById('music').play();
-    content.innerHTML = "PRESS TO PAUSE";
+    startgame.innerHTML = "PRESS TO PAUSE";
   } else {
     //document.getElementById('music').pause();
     window.clearInterval(gameStatus);
     //window.cancelAnimationFrame(gameStatus)
-    content.innerHTML = "PRESS TO START";
+    startgame.innerHTML = "PRESS TO PLAY DEFAULT SONG";
   }
 });
 
 // let gameSpeed = 0;
 // gameSpeed += randomSpeed();
-document.getElementById("speed").innerHTML = '15 ms';
+document.getElementById("speed").innerHTML = '8 ms';
 
 // creating a Grid for reference
 // draw vertical lines
@@ -57,6 +57,13 @@ createText()
 // let wakingUpInVegas = ['GG FF EE DD C E D C', 'CC CC CC CCC E D', 'GG FF EE DD C E D C', 'CC CC CC CCC E D C'];
 // let jingleBells = ['EEE EEE E G C D E', 'FFF FF EE EEE DD E D G', 'EEE EEE E G C D E', 'FFF FF EE EE GG F D C'];
 // let pianoMan = ['GGGG F E F E C', 'CCCC DD', 'E F GGGG F E F E C', 'CCC F E CC']
+
+let maryHadALittleLamb = [
+  'E', '', 'D', '', 'C', '', 'D', '', 'E', '', 'E', '', 'E', '', '', 'D', '', 'D', '', 'D', '', '', 'E', '', 'G', '', 'G', '', '',
+  'E', '', 'D', '', 'C', '', 'D', '', 'E', '', 'E', '', 'E', '', 'E', '', 'D', '', 'D', '', 'E', '', 'D', '', 'C', '', '', '', '',
+  'E', 'D', 'C', 'D', 'E', 'E', 'E', '', 'D', 'D', 'D', '', 'E', 'G', 'G', '',
+  'E', 'D', 'C', 'D', 'E', 'E', 'E', 'E', 'D', 'D', 'E', 'D', 'C', '', '', '',
+]
 
 let pianoMan = [
   'G', 'G', 'G', 'G', '', 'F', '', 'E', '', 'F', '', 'E', '', 'C', '',
@@ -89,6 +96,30 @@ let shutUpandDance = [
   'C', 'C', 'C', '', 'C', '', 'C', '', 'C', '', 'C', 'C', '', 'G', 'G', '', 'E', '', 'D', 'C', 'C'
 ];
 
+let allSongsInOne = [
+  'C', '', 'D', '', 'E', '', 'F', '', 'G', '', '', 'G', 'F', 'E', 'D', 'C', '', '', '',
+  'E', '', 'D', '', 'C', '', 'D', '', 'E', '', 'E', '', 'E', '', '', 'D', '', 'D', '', 'D', '', '', 'E', '', 'G', '', 'G', '', '',
+  'E', '', 'D', '', 'C', '', 'D', '', 'E', '', 'E', '', 'E', '', 'E', '', 'D', '', 'D', '', 'E', '', 'D', '', 'C', '', '', '', '',
+  'E', 'D', 'C', 'D', 'E', 'E', 'E', '', 'D', 'D', 'D', '', 'E', 'G', 'G', '',
+  'E', 'D', 'C', 'D', 'E', 'E', 'E', 'E', 'D', 'D', 'E', 'D', 'C', '', '', '',
+  'E', 'E', 'E', '', 'E', 'E', 'E', '', 'E', '', 'G', '', 'C', '', 'D', '', 'E', '',
+  'F', 'F', 'F', '', 'F', 'F', '', 'E', 'E', '', 'E', 'E', 'E', '', 'D', 'D', '', 'E', '', 'D', '', 'G', '',
+  'E', 'E', 'E', '', 'E', 'E', 'E', '', 'E', '', 'G', '', 'C', '', 'D', '', 'E', '',
+  'F', 'F', 'F', '', 'F', 'F', '', 'E', 'E', '', 'E', 'E', '', 'G', 'G', '', 'F', '', 'D', '', 'C', '', '',
+  'G', 'G', '', 'F', 'F', '', 'E', 'E', '', 'D', 'D', '', 'C', '', 'E', '', 'D', '', 'C', '',
+  'C', 'C', '', 'C', 'C', '', 'C', 'C', '', 'C', 'C', 'C', '', 'E', '', 'D', '',
+  'G', 'G', '', 'F', 'F', '', 'E', 'E', '', 'D', 'D', '', 'C', '', 'E', '', 'D', '', 'C', '',
+  'C', 'C', '', 'C', 'C', '', 'C', 'C', '', 'C', 'C', 'C', '', 'E', '', 'D', '', 'C', '', '',
+  'C', '', 'E', '', 'C', '', 'D', 'D', '', 'E', 'C', 'C', 'C', 'C', '', 'G', 'G', '', 'E', '',
+  'D', 'C', '', 'E', '', 'C', '', 'D', 'D', '', 'E', '', 'D', '', 'C', '', 'D', '', 'E', '', 'C', '', '',
+  'C', 'C', 'C', '', 'C', '', 'C', '', 'C', '', 'C', 'C', 'C', '', 'C', '', 'C', '', 'D', '',
+  'C', 'C', 'C', '', 'C', '', 'C', '', 'C', '', 'C', 'C', '', 'G', 'G', '', 'E', '', 'D', 'C', 'C', '', '',
+  'G', 'G', 'G', 'G', '', 'F', '', 'E', '', 'F', '', 'E', '', 'C', '',
+  'C', 'C', 'C', 'C', '', 'D', 'D', '', 'E', '', 'F', '',
+  'G', 'G', 'G', 'G', '', 'F', '', 'E', '', 'F', '', 'E', '', 'C', '',
+  'C', 'C', 'C', '', 'F', '', 'E', '', 'C', 'C', ''
+]
+
 // splice songs into arrays of 5 letters
 function spliceIntoFive(arr, chunkSize) {
   const res = [];
@@ -98,15 +129,15 @@ function spliceIntoFive(arr, chunkSize) {
   }
   return res
 }
-let test = spliceIntoFive(jingleBells, 5)
+let inputSong = spliceIntoFive(allSongsInOne, 5)
 
 // sort the song letters arrays
 function prepList(input) {
   let list = [];
   const length = input.length; // this was added
   for (let i = 0; i < length; i++) {
-    list.push(addSpace(test));
-    test.shift();
+    list.push(addSpace(input));
+    input.shift();
   }
   let merged = [].concat.apply([], list);
   //console.log(merged);
@@ -114,7 +145,7 @@ function prepList(input) {
   return merged;
 }
 
-let newSong = prepList(test);
+let newSong = prepList(inputSong);
 console.log(newSong.length)
 
 function addSpace(arr) {
@@ -146,13 +177,6 @@ function addSpace(arr) {
     return newList
   }
 }
-
-// function startScene() {
-// }
-
-// function instructionScene() {
-// }
-
 
 // create a class for constructing components
 class Component {
@@ -254,11 +278,11 @@ function createColorList(arr) {
 }
 
 const newColorArr = newSong.map(createColorList);
-console.log(newColorArr)
-console.log(newColorArr.length)
+// console.log(newColorArr)
+// console.log(newColorArr.length)
 
 
-// fixed x positions for every five components
+// fixed x positions for every five groups of components
 const x_input = [
   [cw * 0.1 - 25, cw * 0.3 - 25, cw * 0.5 - 25, cw * 0.7 - 25, cw * 0.9 - 25],
   [cw * 0.1 - 25, cw * 0.3 - 25, cw * 0.5 - 25, cw * 0.7 - 25, cw * 0.9 - 25],
@@ -267,7 +291,7 @@ const x_input = [
   [cw * 0.1 - 25, cw * 0.3 - 25, cw * 0.5 - 25, cw * 0.7 - 25, cw * 0.9 - 25]
 ]
 
-// Generating xPos for constructing components
+// Generating correct # of xPos for corresponding song length
 function fillArray(value, len) {
   let posX = [];
   for (let i = 0; i < len; i++) {
@@ -283,6 +307,7 @@ let startPosY = [0, 0, 0, 0, 0]
 
 function updatePosY(input) {
   return input - 60;
+  // minus y-position by 60 for each component
 }
 
 function generatePosY() {
@@ -294,8 +319,25 @@ function generatePosY() {
   }
   return allPosY
 }
-let yPos = generatePosY();
+let yPos = generatePosY(); // nested arrays of all y-coordinates
 console.log(yPos)
+
+// create a position to check for ''
+function checkPressCondition(input) {
+  return input - 450;
+}
+
+function generateCorrectPress() {
+  let allCorrectPosY = []
+  for (let i = 0; i < newSong.length; i++) {
+    let newArray = startPosY.map(checkPressCondition)
+    startPosY = newArray
+    allCorrectPosY.push(newArray)
+  }
+  return allCorrectPosY
+}
+let correctPos = generateCorrectPress();
+console.log(correctPos);
 
 function createComponents() {
   let width = 50;
@@ -330,7 +372,7 @@ class TrackLines {
     ctx.closePath();
   }
 }
-//constructing some tracklines
+// constructing 5 tracklines
 function createLines(num) {
   for (let i = 0; i < num; i++) {
     let x1 = [cw * 0.2, cw * 0.4, cw * 0.6, cw * 0.8, cw];
@@ -358,33 +400,7 @@ function animateGame() {
   }
 }
 console.log(gameBlocks)
-console.log(gameBlocks.length)
-
-
-
-
-
-
-// const element = document.getElementById('key');
-// let start;
-// function step(timestamp) {
-//   if (start === undefined)
-//     start = timestamp;
-//   const elapsed = timestamp - start;
-
-//   // `Math.min()` is used here to make sure that the element stops at exactly 400px.
-//   element.style.transform = 'translateY(' + Math.min(0.1 * elapsed, 400) + 'px)';
-
-//   if (elapsed < 4000) { // Stop the animation after 2 seconds
-//     window.requestAnimationFrame(step);
-//   }
-// }
-
-// window.requestAnimationFrame(step);
-
-
-
-
+// console.log(gameBlocks.length)
 
 
 // Add Time
@@ -393,10 +409,13 @@ function myTimer() {
   document.getElementById("time").innerHTML = d.toLocaleTimeString('it-IT');
 }
 setInterval(myTimer, 0);
-// EventListeners
-let numOfButtons = document.querySelectorAll("#key").length;
 
-//mouse - click eventlistener
+// Add EventListeners
+let numOfButtons = document.querySelectorAll("#key").length;
+let selectBtn = document.querySelectorAll("#key");
+
+
+// mouse - click eventlistener
 for (let i = 0; i < numOfButtons; i++) {
   document.querySelectorAll("#key")[i].addEventListener("click", function (event) {
     for (let i = 0; i < 1; i++) {
@@ -405,7 +424,7 @@ for (let i = 0; i < numOfButtons; i++) {
         makeCorrectSound(buttonInnerHTML);
         buttonAnimation(buttonInnerHTML);
         addScore(1)
-      } else if (gameBlocks[i].y < 0.8 * ch && gameBlocks[i].y > 0.9 * ch) {
+      } else if (gameBlocks[i].y > ch) {
         makeWrongSound(buttonInnerHTML);
 
       }
@@ -413,34 +432,44 @@ for (let i = 0; i < numOfButtons; i++) {
   }, false);
 }
 
+console.log(gameBlocks[0].y)
+
 // keypress eventlistener
-document.addEventListener("keydown", function (event) {
+document.addEventListener("keydown", (event) => {
   for (let i = 0; i < 1; i++) {
-    if (gameBlocks[i].y >= 0.85 * ch || gameBlocks[i].y <= ch) {
+    if (gameBlocks[i].y <= ch) {
       console.log(event.key)
       makeCorrectSound(event.key);
       buttonAnimation(event.key);
       addScore(1);
-      //console.log(`${event.key}: correct`)
-      // } else makeWrongSound(event.key);
-      // console.log(`${event.key}: wrong`)
-    } else if (gameBlocks[i].y < 0.8 * ch && gameBlocks[i].y > 0.9 * ch) {
-      //makeWrongSound(event.key);
-      addScore(1);
-    }
+      //event.stopPropagation();
+      console.log(`${event.key}: correct`)
+    } else
+      makeWrongSound(event.key);
+    addScore(-1);
   }
 }, false);
 
+// Create Own Songs Section
+function collectSong() {
+  let createBtn = document.querySelectorAll("#create");
+  for (let i = 0; i < createBtn.length; i++) {
+    createBtn[i].addEventListener("click", function (event) {
+      let buttonInnerHTML = this.innerHTML
+      let target = document.getElementById("createSong");
+      const newContent = document.createTextNode(buttonInnerHTML)
+      let newSong = target.append(newContent)
+      console.log(newSong)
+    });
+  }
+  return newSong
+}
 
-// const keys = document.querySelectorAll('#key');
-// keys.forEach(key => {
-//   key.addEventListener('click', () => makeCorrectSound(key.innerHTML))
-// })
-
+let abc = collectSong()
+console.log(abc)
 
 
 function makeCorrectSound(key) {
-
   switch (key) {
     case "a":
       let btnA = new Audio("notes/notes_C4.mp3");
@@ -453,18 +482,18 @@ function makeCorrectSound(key) {
       break;
 
     case "d":
-      let btnD = new Audio('notes/notes_E4.mp3');
-      btnD.play();
+      let btnC = new Audio('notes/notes_E4.mp3');
+      btnC.play();
       break;
 
     case "f":
-      let btnF = new Audio('notes/notes_F4.mp3');
-      btnF.play();
+      let btnD = new Audio('notes/notes_F4.mp3');
+      btnD.play();
       break;
 
     case "g":
-      let btnH = new Audio('notes/notes_G4.mp3');
-      btnH.play();
+      let btnE = new Audio('notes/notes_G4.mp3');
+      btnE.play();
       break;
 
     default:
@@ -505,7 +534,7 @@ function makeWrongSound(key) {
   }
 }
 
-// some button effects by changing css class
+// button when clicked also changes class for 2ms
 function buttonAnimation(key) {
   let activeButton = document.querySelector("." + key);
   activeButton.classList.add("pressed_" + key);
@@ -548,3 +577,17 @@ function addScore(x) {
   });
 
 }
+
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function (e) {
+//   if (!e.target.matches('.dropbtn')) {
+//     var myDropdown = document.getElementById("myDropdown");
+//     if (myDropdown.classList.contains('show')) {
+//       myDropdown.classList.remove('show');
+//     }
+//   }
+// }
